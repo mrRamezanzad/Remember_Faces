cf221667ee038e6cea246e2d41efbff093f8d439
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2021-06-15 12:15:49 +0430

    made the faces game UI better

--- public/javascripts/faces.js
+++ public/javascripts/faces.js
@@ -1,16 +1,27 @@
 (function($) {
     const cards = $('.card')
     const timer = $('.timer')
-    let counter = 15
+    // let counter = 15
+    let counter = 5000000
+
+    // setInterval(() => {
+    //     if (counter === 0 ) {
+    //         clearInterval()
+    //         return cards.next('label').text('')
+    //     }
+    //     --counter
+    //     timer.text(counter)
+    // },1000);
 
     setInterval(() => {
-        if (counter === 0 ) {
+        if (counter <= 0 ) {
             clearInterval()
             return cards.next('label').text('')
         }
-        --counter
-        timer.text(counter)
-    },1000);
+        const decreament = Math.floor(Math.random()*90000)
+        counter-=decreament
+        timer.text(parseInt(timer.text())+decreament)
+    },50);
  
 
 })(jQuery)
