634bab0dc6b9f4798400bf597d18b751200c5d6a
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2021-07-13 19:39:03 +0430

    changes: - added timer functionality to make the game more chanllenging - fixed some bugs

--- public/javascripts/faces.js
+++ public/javascripts/faces.js
@@ -1,20 +1,24 @@
 (function($) {
-
+    
 const cards = $('.card')
-const nameTags = cards.siblings('p')
 const answerInputs = cards.siblings('input')
-// const gameStartDelay = 5000 //45000
-const memorizingDuration = 5000 //120000
-const answeringDuration = 5000 //120000
+// const gameStartDelay = 5000 
+const memorizingDuration = 30 //seconds
+const answeringDuration = 40 //seconds
 
 // ============================== prepare UI for answering, then check them with server
+startTimer(memorizingDuration)
 setTimeout(() => {
-     toggleNames()
-     toggleAnswerInputs()
-     setTimeout(checkAnswersWithServer, answeringDuration)
-}, memorizingDuration)
+    
+    
+    $('#timer-alarm').html('Remaning time to answer <span class="timer text-danger">-</span> seconds ')
+    toggleNames()
+    toggleAnswerInputs()
+    startTimer(answeringDuration)
+    setTimeout(checkAnswersWithServer, answeringDuration*1000)
+}, memorizingDuration*1000+2000) // adding two more seconds to fix time jumping bug
 
-function toggleNames () { nameTags.toggleClass('d-none') }
+function toggleNames () { const nameTags = cards.siblings('p'); nameTags.toggleClass('d-none') }
 
 function toggleAnswerInputs () { answerInputs.toggleClass('d-none') }
 
@@ -49,55 +53,25 @@ function showGameResults (results) {
     let resultsLength = results.length
     for (; answerIndex < resultsLength; answerIndex++) {
         if (results[answerIndex])
-            $(answerInputs[answerIndex]).css("border", "1px solid green")
+            $(answerInputs[answerIndex]).css({"border": "1px solid green", "border-radius": "5px", "text-align": "center"})
             
-        else $(answerInputs[answerIndex]).css("border", "1px solid orangered")
+        else $(answerInputs[answerIndex]).css({"border": "1px solid orangered", "border-radius": "5px", "text-align": "center"})
     }
 }
 
-// TODO: ADDING TIMER PREVIEW TO PAGE
-// const timerInput = $('.timer')
-// let counter = 15
-// let counter = 5000000
+// ============================= Timer =========================
+// Showing timer so user knows how much time has to memorize and answer 
 
-// setInterval(() => {
-//     if (counter === 0 ) {
-//         clearInterval()
-//         return cards.next('label').text('')
-//     }
-//     --counter
-//     timer.text(counter)
-// },1000);
+function startTimer (showingTime) {
 
-// ============================== showing answer inputs
-// setInterval(() => {
-    //     if (counter <= 0 ) {
-        //         clearInterval()
-        //         cards.next('p').text('')
-        //         
-        //         return 
-//     }
-//     const decreament = Math.floor(Math.random()*90000)
-//     counter-=decreament
-//     timerInput.text(parseInt(timerInput.text())+decreament)
-// },50);
+    const timerInput = $('.timer')
+    let timerInterval = setInterval(() => {
 
- 
+        timerInput.text(showingTime)
+        showingTime--
+        if (showingTime < 0) clearInterval(timerInterval)
+        
+    }, 1000)
+}
+
 })(jQuery)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
